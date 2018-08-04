0acd34be9c8190a8e0e2e968196cf4303b37caa1
Akshay Anandrao Jagadale <aksbret1990@gmail.com>
2018-08-04 10:29:08 -0700

    Add files via upload

---
+++ update example.sql
@@ -0,0 +1,18 @@
+
+
+--update co 
+set co.Value = concat('715',co.Value)
+--select co.Value, concat('715',co.Value)
+from
+	schemaname.contacttable as co
+where
+	co.TYPE = 0
+	AND LEN(co.[value]) < 10
+--
+
+
+
+
+
+
+
